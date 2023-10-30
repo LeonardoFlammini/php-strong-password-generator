5abb19eedebbabd145a2bc6aef4e5ed2e1599d44
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-10-30 09:59:55 +0100

    [add] scaffolding

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$s = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
+$min=8;
+$max=32;
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css' integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==' crossorigin='anonymous'/>
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP Strong Password Generator</title>
+</head>
+<body>
+  <div class="container-lf my-5">
+    <div class="row row-cols-1 align-items-center text-center " >
+      <h1>Password Generator</h1>
+      <div class="alert alert-success my-3" role="alert">
+        Scegli una password da 8 a 32 caratteri
+      </div>
+    </div>
+    <form action="index.php" metho="GET">
+    
+    </form>
+  </div>
+  
+</body>
+</html>
